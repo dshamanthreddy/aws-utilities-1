cd22fd1b3bafed0a9246ad69e93357ea7756fb2c
tbartwal <tarun.voodoo@gmail.com>
2017-05-08 11:06:12 +1000

    Create list-instance.sh

---
+++ list-instance.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# list all the ec2 instances for your account in all regions.
+
+for region in `aws ec2 describe-regions --output text | cut -f3`
+do
+     echo -e "\nListing Instances in region:'$region'..."
+     aws ec2 describe-instances --region $region --query 'Reservations[*].Instances[*].[InstanceId,State.Name,InstanceType,PrivateIpAddress,PublicIpAddress,Tags[?Key==`Name`].Value[]]' --output json | tr -d '\n[] "' | perl -pe 's/i-/\ni-/g' | tr ',' '\t' | sed -e 's/null/None/g' | grep '^i-' | column -t
+done
